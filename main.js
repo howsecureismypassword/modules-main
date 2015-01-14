e4af057011a43b6bed4cca57d9d14e7e9a9981a4
Mark Wales <mark@smallhadroncollider.com>
2015-01-14 17:26:30 +0000

    added named numbers/periods to output

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 "use strict";
 
 var L = require("library");
+
 var characterSets = require("character-sets");
+var period = require("period");
+var namedNumber = require("named-number");
+
 var defaults = {
     calculationsPerSecond: 1e10
 };
@@ -10,19 +14,29 @@ var options = {};
 
 var hsimp = function (password) {
     var self = {};
-    var possibleCharacters = characterSets(password).getPossibleCharacters();
+
+    var possibleCharactersCheck = characterSets(password);
+    var possibleCharacters = possibleCharactersCheck.getPossibleCharacters();
+
     var possibleCombinations = Math.pow(possibleCharacters, password.length);
+    var timeInSeconds = possibleCombinations / options.calculationsPerSecond;
 
-    self.getPossibleCombinations = function () {
-        return possibleCombinations;
-    };
+    var passwordPeriodCalculator = period(timeInSeconds);
+    var passwordPeriodLength = passwordPeriodCalculator.getLength();
+    var formattedLength = namedNumber(passwordPeriodLength).getName();
 
-    self.getTimeInSeconds = function () {
-        return possibleCombinations / options.calculationsPerSecond;
-    };
+    var periodString = formattedLength + " " + (formattedLength === "1" ? passwordPeriodCalculator.getSingular() : passwordPeriodCalculator.getPlural());
+
+    self.getPossibleCombinations = L.output(possibleCombinations);
+    self.getTimeInSeconds = L.output(timeInSeconds);
+    self.getString = L.output(periodString);
+    self.getTimeString = L.output(periodString);
 
     return self;
 };
+
+hsimp.setPeriodDictionary = period.setDictionary;
+hsimp.setNamedNumberDictionary = namedNumber.setDictionary;
 
 hsimp.setOptions = function (opts) {
     options = L.defaults(defaults, opts);
--- test.js
+++ test.js
@@ -10,6 +10,11 @@ var assert = buster.referee.assert;
  * Setup
  */
 var hsimp = require("./main");
+var periodDictionary = require("period/period-dictionary");
+var namedNumberDictionary = require("named-number/named-number-dictionary");
+
+hsimp.setPeriodDictionary(periodDictionary);
+hsimp.setNamedNumberDictionary(namedNumberDictionary);
 
 hsimp.setOptions({
     "calculationsPerSecond": 1e10
@@ -33,12 +38,33 @@ buster.testCase("hsimp", {
             assert.equals(hsimp("abc123ABC").getPossibleCombinations(), 13537086546263552);
         }
     },
+
     "Time In Seconds": {
         "wkjhfakjsdfh": function () {
             assert.equals(hsimp("wkjhfakjsdfh").getTimeInSeconds(), 9542895.666168218);
         },
         "Mixed Character Sets": function () {
             assert.equals(hsimp("abc123ABC").getTimeInSeconds(), 1353708.6546263553);
+        }
+    },
+
+    "Time In Periods": {
+        "1111111111": function () {
+            assert.equals(hsimp("1111111111").getString(), "1 second");
+        },
+        "monkeycatfish": function () {
+            assert.equals(hsimp("monkeycatfish").getString(), "8 years");
+        },
+        "BowT1esAreC00l": function () {
+            assert.equals(hsimp("BowT1esAreC00l").getString(), "39 million years");
+        },
+        "8RTMRwNpDeP9JZb8ts77@W!fW@JC_Jtw9_Z!8rf#": function () {
+            assert.equals(hsimp("8RTMRwNpDeP9JZb8ts77@W!fW@JC_Jtw9_Z!8rf#").getString(), "9 octodecillion years");
+        }
+    },
+    "Time": {
+        "BowT1esAreC00l": function () {
+            assert.equals(hsimp("BowT1esAreC00l").getTimeString(), "39 million years");
         }
     }
 });
